dbbce4ca527c3223c77dfb713abd4c113dba0c71
yang <hyperyx37@gmail.com>
2020-06-17 15:31:39 -0400

    initial commit

---
+++ Greetings/src/test/java/JavaSpring/Practice/Greetings/GreetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaSpring.Practice.Greetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
